4b321b25c4f79605eab61e7c5a3fac602494a004
wang-zi-tao <2451355023@qq.com>
2019-11-13 22:32:52 +0800

    添加json配置

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import os
 import glob
 import random
 import keras
+import json
 import numpy as np
 import pandas as pd
 from keras.preprocessing import image
@@ -16,15 +17,17 @@ import matplotlib.pyplot as plt
 config = ConfigProto()
 config.gpu_options.allow_growth = True
 session = InteractiveSession(config=config)
-
+'''
 default_train_times=16
 model_file='./model/model.h5'
 predict_result_file='./predict_result.csv'
 train_dir = "./验证码图片内容识别竞赛数据/train/"
 test_dir = "./验证码图片内容识别竞赛数据/test/"
-image_shape=(40,120,3)
+
 test_rate=0.1
 batch_size = 128
+'''
+image_shape=(40,120,3)
 def compare(predict,label):
     n=predict[0].shape[0]
     '''
@@ -66,7 +69,8 @@ def base64_char_to_int(char):
         return c-97+36
         
 # 加载训练图片和验证码文字
-def load_train_picture_and_label():
+def load_train_picture_and_label(config):
+
     train_label_c0=[]
     train_label_c1=[]
     train_label_c2=[]
@@ -74,12 +78,14 @@ def load_train_picture_and_label():
 
     train_images = []
 
+    image_dir=config['源图路径']
+
     random.seed()
-    pd_data = pd.read_csv(train_dir+'train_label.csv')
+    pd_data = pd.read_csv(image_dir+'train_label.csv')
     for i in range(0, pd_data.shape[0]):
         file = pd_data.iloc[i, 0]
         label = pd_data.iloc[i, 1]
-        image_data = image.load_img(train_dir+file)
+        image_data = image.load_img(image_dir+file)
         image_tensor = image.img_to_array(image_data)
         label_tensor_0=int_to_tensor(base64_char_to_int(label[0]),62)
         label_tensor_1=int_to_tensor(base64_char_to_int(label[1]),62)
@@ -103,17 +109,19 @@ def load_train_picture_and_label():
     return train_images, train_labels
 
 # 加载测试图片和文件名
-def load_predict_pictrue():
+def load_predict_pictrue(config):
     image_name_list = []
     image_data_list = []
+    image_dir=config['源图路径']
     for i in range(1,5001):
         file=str(i)+'.jpg'
         image_name_list.append(file)
-        image_data = image.load_img(test_dir+file)
+        image_data = image.load_img(image_dir+file)
         image_tensor = image.img_to_array(image_data)
         image_data_list.append(image_tensor)
     image_data_list=np.array(image_data_list,dtype='float')/255.0
     return image_data_list, image_name_list
+
 class LossHistory(keras.callbacks.Callback):
     def on_train_begin(self, logs={}):
         self.losses = {'batch':[], 'epoch':[]}
@@ -152,39 +160,49 @@ class LossHistory(keras.callbacks.Callback):
         plt.show()
 
 def main():
-    if True:
-        if os.path.exists(model_file):
-            model=load_model()
+    config=json.loads(open('./config.json',encoding='utf-8').read())
+    if config['模型']['放弃']:
+        model=creat_model(image_shape)
+    else:
+        if os.path.exists(config['模型']['文件路径']):
+            model=load_model(config['模型'])
         else:
             model=creat_model(image_shape)
-    else:
-        model=creat_model(image_shape)
-    if True:
-        train_images, train_labels=load_train_picture_and_label()
-        while True:
-            train(model,train_images, train_labels,times=64)
-            store_model(model)
-    if False:
-        predict_image,predict_file_name=load_predict_pictrue()
+    if config['训练']['启用']:
+        train_config=config['训练']
+        train_images, train_labels=load_train_picture_and_label(train_config['训练集'])
+        if train_config['循环训练']:
+            while True:
+                train(model,train_images, train_labels,train_config)
+                store_model(model,config['模型'])
+        else:
+            train(model,train_images, train_labels,train_config)
+            store_model(model,config['模型'])
+    if config['预测']['启用']:
+        predict_config=config['预测']
+        predict_image,predict_file_name=load_predict_pictrue(predict_config['预测集'])
         predict_label=predict(model,predict_image)
-        save_predict_result(predict_file_name,predict_label)
+        save_predict_result(predict_file_name,predict_label,predict_config)
 
-    
+
 
 # 构建模型
 def creat_model(input_shape):
-    pic_in = Input(shape=input_shape)
-    cnn_features = Conv2D(32, (5,5), activation='relu',kernel_initializer='he_uniform')(pic_in)
+    cnn_features = pic_in = Input(shape=input_shape)
+    cnn_features = GaussianDropout(0.4)(cnn_features)
+
+    cnn_features = Conv2D(32, (5,5), activation='relu',kernel_initializer='he_uniform')(cnn_features)
     cnn_features = Conv2D(32, (5,5), activation='relu',kernel_initializer='he_uniform')(cnn_features)
     cnn_features = MaxPooling2D((2, 2))(cnn_features)
-    #cnn_features = Dropout(0.25)(cnn_features)
+    cnn_features = Dropout(0.25)(cnn_features)
     cnn_features = Conv2D(48, (3,3), activation='relu',kernel_initializer='he_uniform')(cnn_features)
-    cnn_features = Conv2D(64, (3,3), activation='relu',kernel_initializer='he_uniform')(cnn_features)
+    cnn_features = Conv2D(48, (3,3), activation='relu',kernel_initializer='he_uniform')(cnn_features)
     cnn_features = MaxPooling2D((2, 2))(cnn_features)
+    cnn_features = Dropout(0.25)(cnn_features)
     cnn_features = Conv2D(96, (3,3), activation='relu',kernel_initializer='he_uniform')(cnn_features)
     cnn_features = Conv2D(128, (3,3), activation='relu',kernel_initializer='he_uniform')(cnn_features)
     cnn_features = MaxPooling2D((2, 2))(cnn_features)
-    #cnn_features = Dropout(0.25)(cnn_features)
+    cnn_features = Dropout(0.25)(cnn_features)
     #cnn_features = Conv2D(64, (5,5), activation='relu')(cnn_features)
     #cnn_features = Conv2D(64, (5,5), activation='relu')(cnn_features)
     #cnn_features = MaxPooling2D((2, 2))(cnn_features)
@@ -203,52 +221,107 @@ def creat_model(input_shape):
     cnn_features = Dropout(0.25)(cnn_features)
     '''
     # classifier 1
-    output_l1 = Dense(128, activation='relu',kernel_initializer='he_uniform')(cnn_features)
+    output_l1 = Dense(128, activation='relu',kernel_initializer='he_uniform',kernel_regularizer=regularizers.l2(0.01))(cnn_features)
     output_l1 = Dropout(0.25)(output_l1)
-    output_l1 = Dense(62, activation='softmax',kernel_initializer='he_uniform')(output_l1)
+    output_l1 = Dense(62, activation='softmax',kernel_initializer='he_uniform',kernel_regularizer=regularizers.l2(0.01))(output_l1)
     # classifier 2
-    output_l2 = Dense(128, activation='relu',kernel_initializer='he_uniform')(cnn_features)
+    output_l2 = Dense(128, activation='relu',kernel_initializer='he_uniform',kernel_regularizer=regularizers.l2(0.01))(cnn_features)
     output_l2 = Dropout(0.25)(output_l2)
-    output_l2 = Dense(62, activation='softmax',kernel_initializer='he_uniform')(output_l2)
+    output_l2 = Dense(62, activation='softmax',kernel_initializer='he_uniform',kernel_regularizer=regularizers.l2(0.01))(output_l2)
     # classifier 3
-    output_l3 = Dense(128, activation='relu',kernel_initializer='he_uniform')(cnn_features)
+    output_l3 = Dense(128, activation='relu',kernel_initializer='he_uniform',kernel_regularizer=regularizers.l2(0.01))(cnn_features)
     output_l3 = Dropout(0.25)(output_l3)
-    output_l3 = Dense(62, activation='softmax',kernel_initializer='he_uniform')(output_l3)
+    output_l3 = Dense(62, activation='softmax',kernel_initializer='he_uniform',kernel_regularizer=regularizers.l2(0.01))(output_l3)
     # classifier 4
-    output_l4 = Dense(128, activation='relu',kernel_initializer='he_uniform')(cnn_features)
+    output_l4 = Dense(128, activation='relu',kernel_initializer='he_uniform',kernel_regularizer=regularizers.l2(0.01))(cnn_features)
     output_l4 = Dropout(0.25)(output_l4)
-    output_l4 = Dense(62, activation='softmax',kernel_initializer='he_uniform')(output_l4)
+    output_l4 = Dense(62, activation='softmax',kernel_initializer='he_uniform',kernel_regularizer=regularizers.l2(0.01))(output_l4)
 
 
     model = Model(inputs=pic_in, outputs=[output_l1, output_l2, output_l3, output_l4])
 
     model.compile(
-              optimizer=Adagrad(),
+              optimizer=Adadelta(),
               loss='categorical_crossentropy',
-              loss_weights=[1., 1., 1., 1.], 
+              loss_weights=[1., 1.2, 1.2, 1.], 
               metrics=['accuracy'])
 
     return model
 
 
-def train(model,train_images, train_labels,times=default_train_times):
-    history=LossHistory()
+def train(model,train_images, train_labels,config):
     callbacks = [
-        history
+        EarlyStopping(patience=5, verbose=1, mode='auto')
     ]
-    model.fit(train_images,train_labels,
-          epochs=times,
-          #batch_size=batch_size,
-          #validation_split=test_rate,
-          verbose=1,
-          callbacks=callbacks,
-          validation_split=0.1
-          )
-    print(compare(model.predict(train_images[:-500]),[p[:-500] for p in train_labels]))
-    print(compare(model.predict(train_images[-500:]),[p[-500:] for p in train_labels]))
-
-    history.loss_plot('epoch')
-    return -1
+    if config['显示图形']:
+        callbacks_history=LossHistory()
+        callbacks.append(callbacks_history)
+    
+    if config['扩充']:
+        
+        def expansion_train_images(images,labels):
+            i=0
+            c=50
+            while True:
+                matrix=np.random.rand(3,3)/3
+                for r in range(0,100):
+                    image=images[i:i+c]
+                    image=np.matmul(image,matrix)
+                    yield (image,[labels[0][i:i+c],labels[1][i:i+c],labels[2][i:i+c],labels[3][i:i+c]])
+                    i+=c
+                    if i>=4500:
+                        i=0
+        def expansion_test_images(images,labels):
+            i=4500
+            c=50
+            while True:
+                for r in range(0,100):
+                    image=images[i:i+c]
+                    yield (image,[labels[0][i:i+c],labels[1][i:i+c],labels[2][i:i+c],labels[3][i:i+c]])
+                    i+=c
+                    if i>=5000:
+                        i=4500
+        model.fit_generator(
+              expansion_train_images(train_images,train_labels),
+              steps_per_epoch=800,
+              epochs=config['次数'],
+              batch_size=config['批量数目'],
+              validation_data=expansion_test_images(train_images,train_labels),
+              validation_steps=5,
+              verbose=config['显示模式'],
+              callbacks=callbacks,
+        )
+        #print(compare(model.predict(np.matmul(train_images[-500:],np.random.rand(3,3)/3)),[p[-500:] for p in train_labels]))
+        print(compare(model.predict(train_images[:-500]),[p[:-500] for p in train_labels]))
+        print(compare(model.predict(train_images[-500:]),[p[-500:] for p in train_labels]))
+        '''
+        model.fit(np.matmul(train_images,np.random.rand(3,3)/3),
+              train_labels,
+              epochs=config['次数'],
+              batch_size=config['批量数目'],
+              #validation_split=test_rate,
+              verbose=config['显示模式'],
+              callbacks=callbacks,
+              validation_split=0.1
+              )
+        #print(compare(model.predict(np.matmul(train_images[:-500],np.random.rand(3,3)/3)),[p[:-500] for p in train_labels]))
+        
+        print(compare(model.predict(train_images[:-500]),[p[:-500] for p in train_labels]))
+        print(compare(model.predict(train_images[-500:]),[p[-500:] for p in train_labels]))
+        '''
+    else:
+        model.fit(train_images,train_labels,
+              epochs=config['次数'],
+              batch_size=config['批量数目'],
+              #validation_split=test_rate,
+              verbose=config['显示模式'],
+              callbacks=callbacks,
+              validation_split=0.1
+              )
+        print(compare(model.predict(train_images[:-500]),[p[:-500] for p in train_labels]))
+        print(compare(model.predict(train_images[-500:]),[p[-500:] for p in train_labels]))
+    if config['显示图形']:
+        callbacks_history.loss_plot('epoch')
 
 def predict(model,images):
     predict_tensor=model.predict(images)
@@ -261,15 +334,16 @@ def predict(model,images):
             label[-1]+=(int_to_base64_char(label_number))
     return label
 
-def save_predict_result(file_names,labels):
+def save_predict_result(file_names,labels,config):
     frame = pd.DataFrame({'ID':file_names,'label':labels},columns=['ID','label'])
-    frame.to_csv(predict_result_file)
+    frame.to_csv(config['预测结果文件路径'])
 
-def load_model():
-    return keras.models.load_model(model_file)
+def load_model(config):
+    return keras.models.load_model(config['文件路径'])
 
 
-def store_model(model):
-    model.save(model_file)
+def store_model(model,config):
+    model.save(config['文件路径'])
+
 if __name__ == "__main__":
     main()
